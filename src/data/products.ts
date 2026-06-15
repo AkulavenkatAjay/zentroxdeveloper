8e0d30db8bb7c1b6deb7bd90bab1bc0e6ec189f5
AkulavenkatAjay <104056317+AkulavenkatAjay@users.noreply.github.com>
2026-06-15 13:33:00 +0530

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -13,7 +13,7 @@ const products: Array<Product> = [
     name: 'Product 1',
     image: '/placeholder.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      'Lorem ipsum dolor sit amet, consectetur iscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     shortDescription: 'A generic product description for your first product.',
     price: 3000,
   },
